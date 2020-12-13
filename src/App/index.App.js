d290a5489117b422b29b51797f1127d6de783cdb
Kroh Nickolay <375336623761@ya.ru>
2020-12-13 23:02:30 +0300

    feat: add App module

---
+++ src/App/index.App.js
@@ -0,0 +1,15 @@
+import Table from '../Table/index.Table';
+import Observer from '../Observer/index.Observer';
+
+export default class App {
+  constructor() {
+    const parentElement = document.body;
+    this.observer = new Observer();
+
+    this.table = new Table(parentElement, this.observer);
+  }
+
+  async start() {
+    this.table.start();
+  }
+}
