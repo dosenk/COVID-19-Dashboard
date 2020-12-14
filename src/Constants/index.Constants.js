f642211b34b67e9f8194208685bd74a19dd8c13f
Kroh Nickolay <375336623761@ya.ru>
2020-12-14 18:03:35 +0300

    refactor: add Constants module

---
+++ src/Constants/index.Constants.js
@@ -0,0 +1,3 @@
+export const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
+export const COVID_API = 'https://api.covid19api.com';
+export const COUNTRY_INFO_API = 'https://restcountries.eu/rest/v2';
