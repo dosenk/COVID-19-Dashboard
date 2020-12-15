242b2c07fb9d29d00c4a52230c4af096f677b788
Kroh Nickolay <375336623761@ya.ru>
2020-12-15 13:25:49 +0300

    refactor: add default constants country and dataType

--- src/Constants/index.Constants.js
+++ src/Constants/index.Constants.js
@@ -1,3 +1,7 @@
+import { TOTAL_CONFIRMED } from '../Observer/dataTypes';
+
 export const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
 export const COVID_API = 'https://api.covid19api.com';
 export const COUNTRY_INFO_API = 'https://restcountries.eu/rest/v2';
+export const DEFAULT_DATA_TYPE = TOTAL_CONFIRMED;
+export const DEFAULT_COUNTRY = 'ALL';
