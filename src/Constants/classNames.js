8e0e5a74624788fc9734eeaecc34c1f86b8d5037
Kroh Nickolay <375336623761@ya.ru>
2020-12-22 20:43:52 +0300

    fix: wrappwer classNames orelaps with components names

--- src/Constants/classNames.js
+++ src/Constants/classNames.js
@@ -1,4 +1,4 @@
-export const TABLE_CLASSNAME = 'table';
-export const MAP_CLASSNAME = 'map';
-export const CHART_CLASSNAME = 'chart';
-export const LIST_CLASSNAME = 'list';
+export const TABLE_CLASSNAME = 'table--wrapper';
+export const MAP_CLASSNAME = 'map--wrapper';
+export const CHART_CLASSNAME = 'chart--wrapper';
+export const LIST_CLASSNAME = 'list--wrapper';
