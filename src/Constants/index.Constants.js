0a296ffaa0740139f57a050cec918847ece7b0b2
Dmitry Osipenko <eympel@gmail.com>
2020-12-22 12:58:33 +0300

    fix: changed the error when hovering over the block with information about the country, changed the names of countries in the geojson data

--- src/Constants/index.Constants.js
+++ src/Constants/index.Constants.js
@@ -1,9 +1,5 @@
 import { TOTAL_CONFIRMED } from './dataTypes';
 
-export const MAPBOX_API =
-  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXltcGVsIiwiYSI6ImNraXJpeWNreTFzZGozM3AzaGx4cDIxbGwifQ.eiHcaBfQAFrV6anXy3V8mA';
-export const OPENSTRETMAP_API =
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 export const DEFAULT_DATA_TYPE = TOTAL_CONFIRMED;
 export const GLOBAL_SELECTOR = 'All';
 export const DEFAULT_COUNTRY = GLOBAL_SELECTOR;
