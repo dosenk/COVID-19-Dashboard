29aec8f4f887aa0450f0c786fe67767c425bd0e0
beafraidnotlearning <beafraidnotlearning@mail.ru>
2020-12-23 16:01:19 +0300

    feat: implement a method for filling headers and a method for selecting characteristics

--- src/Components/List/index.List.js
+++ src/Components/List/index.List.js
@@ -31,6 +31,73 @@ export default class List {
     }, 2000);
   }
 
+  addCheckbox() {
+    const div = document.createElement('div');
+    div.className = 'checkbox-list';
+    this.array.forEach((el, index) => {
+      const label = document.createElement('label');
+      const input = document.createElement('input');
+      input.type = 'checkbox';
+      input.className = `checkbox${el}`;
+      label.innerText += el;
+      input.checked = true;
+      input.addEventListener('click', () => {
+        this.fillContent();
+      });
+      label.appendChild(input);
+      div.appendChild(label);
+    });
+    this.section.appendChild(div);
+  }
+
+  createListTitles() {
+    const ul = document.createElement('ul');
+    ul.style.display = 'flex';
+    this.array.forEach((keyName, keyIndex) => {
+      if (document.querySelector(`.checkbox${keyName}`).checked) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const aUp = document.createElement('a');
+        const aDown = document.createElement('a');
+        li.className = `title-${keyName}`;
+        a.innerHTML = `${keyName.replace('Name', 'Country')}`; // &uArr; &dArr;
+        aUp.innerHTML = '&uArr;'; // &uArr; &dArr;
+        aDown.innerHTML = '&dArr;'; // &uArr; &dArr;
+        if (keyIndex) {
+          li.addEventListener('click', (e) => {
+            if (e.target.innerHTML === '⇓') {
+              this.countries = this.countries.sort((a1, b) => {
+                if (a1[keyName.replace('P', 'p')] > b[keyName.replace('Name', 'name').replace('P', 'p')]) {
+                  return 1;
+                }
+                if (a1[keyName.replace('P', 'p')] < b[keyName.replace('Name', 'name').replace('P', 'p')]) {
+                  return -1;
+                }
+                return 0;
+              });
+            } else if (e.target.innerHTML === '⇑') {
+              this.countries = this.countries.sort((a1, b) => {
+                if (a1[keyName.replace('P', 'p')] > b[keyName.replace('Name', 'name').replace('P', 'p')]) {
+                  return -1;
+                }
+                if (a1[keyName.replace('P', 'p')] < b[keyName.replace('Name', 'name').replace('P', 'p')]) {
+                  return 1;
+                }
+                return 0;
+              });
+            }
+            this.fillContent();
+          });
+        }
+        li.appendChild(a);
+        li.appendChild(aUp);
+        li.appendChild(aDown);
+        ul.appendChild(li);
+      }
+    });
+    this.section.appendChild(ul);
+  }
+
   addChangeTypeBtn(type) {
     const btn = document.createElement('button');
     btn.textContent = type;
