a851f0384940f1fd010e4d8a23fca3f4885c1c11
beafraidnotlearning <beafraidnotlearning@mail.ru>
2020-12-23 20:22:48 +0300

    feat: implement methods for list display

--- src/Components/List/index.List.js
+++ src/Components/List/index.List.js
@@ -16,6 +16,57 @@ export default class List {
     }
   }
 
+  createSectionList() {
+    const section = document.createElement('section');
+    section.className = 'list-section';
+    this.parent.appendChild(section);
+    section.style.height = `${window.innerHeight * 0.6}px`;
+    section.style.overflow = 'scroll';
+    this.section = section;
+
+    setTimeout(() => {
+      this.fillList();
+    }, 2000);
+  }
+
+  fillList() {
+    this.addCheckbox();
+
+    const divTextField = document.createElement('div');
+    const textField = document.createElement('input');
+    textField.type = 'textarea';
+    textField.placeholder = 'Enter country';
+    divTextField.appendChild(textField);
+
+    const inputEnter = document.createElement('input');
+    inputEnter.type = 'button';
+    inputEnter.value = 'Search';
+    inputEnter.addEventListener('click', () => {
+      if (this.data.get(textField.value)) {
+        this.getCountry(this.data.get(textField.value));
+      } else if (textField.value === '') {
+        this.fillContent();
+      } else {
+        textField.value = '';
+        textField.placeholder = 'Re-enter country';
+      }
+    });
+    divTextField.appendChild(inputEnter);
+
+    this.section.appendChild(divTextField);
+
+    const divTitles = document.createElement('table');
+    this.divTitles = divTitles;
+    this.section.appendChild(divTitles);
+    this.createListTitles();
+
+    const content = document.createElement('table');
+    this.content = content;
+    this.section.appendChild(content);
+
+    this.fillContent();
+  }
+
   start() {
     this.div = document.createElement('div');
     const input = document.createElement('input');
